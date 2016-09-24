de5b6367eff3307bc01b90214d71d03095c0ea32
Nikhil Vengal <nikhilvengal@gmail.com>
2016-09-24 04:00:33 -0400

    Second Commit Added Rebalancing Portfolio

--- StockSimulation.py
+++ StockSimulation.py
@@ -20,6 +20,15 @@ def calcBondRate():
     stockRate = random.gauss(mu, sigma)
     return stockRate
 
+#Rebalances stock to bond ratio
+def rebalance(valueRStock, valueRBond):
+    percentStock = .9
+    percentBond = .1
+    total = valueRStock + valueRBond
+    valueRStock = total * percentStock
+    valueRBond = total * percentBond
+    return valueRStock, valueRBond
+
 #Calculates value of equity
 def calcValue(rate, value):
     value += rate*value
@@ -30,14 +39,18 @@ def initial():
     valueStock = 9000
     valueBond = 1000
     valuePureStock = 10000
-    return valueStock,valueBond, valuePureStock
+    valueRStock = 9000
+    valueRBond = 1000
+    return valueStock,valueBond, valuePureStock, valueRStock, valueRBond
 
+#Initialize
 y = 0
 yearsPerIteration = 70
 totalIterations = 10000
 pureStockWins = 0
 mixedPortfolioWins = 0
-valueStock, valueBond, valuePureStock = initial()
+rebalancedPortfolioWins = 0
+valueStock, valueBond, valuePureStock, valueRStock, valueRBond = initial()
 
 #Run multiple times
 while y<totalIterations:
@@ -45,25 +58,36 @@ while y<totalIterations:
 #Calculate change in fund value
     while x<yearsPerIteration:
         stockRate = calcStockRate()
+        bondRate = calcBondRate()
         valueStock = calcValue(stockRate, valueStock)
-        valueBond = calcValue(calcBondRate(), valueBond)
+        valueBond = calcValue(bondRate, valueBond)
         valuePureStock = calcValue(stockRate, valuePureStock)
+        valueRStock = calcValue(stockRate, valueRStock)
+        valueRBond = calcValue(bondRate, valueRBond)
+        #print "BEFORE Rebalanced Stock Val = ", valueRStock
+        #print "BEFORE Rebalanced Bond Val = ", valueRBond 
+        valueRStock, valueRBond = rebalance(valueRStock, valueRBond)
         x += 1
 
+        #print "AFTER Rebalanced Stock Val = ", valueRStock
+        #print "AFTER Rebalanced Bond Val = ", valueRBond
     #print "Stock value = ", valueStock, " Bond Value = ", valueBond
     #print "Total Mixed = ", valueStock+valueBond
     #print "Pure Stock = ", valuePureStock
 
-    #Compare mixed to pure portfolios and count
-    if (valueStock + valueBond) > valuePureStock:
+    #Compare mixed to rebalanced to pure portfolios and count
+    if (valueStock + valueBond) > valuePureStock and (valueStock + valueBond) > (valueRStock + valueRBond):
         mixedPortfolioWins += 1
-    if (valueStock + valueBond) < valuePureStock:
+    if valuePureStock > (valueStock + valueBond) and valuePureStock > (valueRStock + valueRBond):
         pureStockWins += 1
+    if (valueRStock + valueRBond) > valuePureStock and (valueRStock + valueRBond) > (valueStock + valueBond):
+        rebalancedPortfolioWins += 1
     y += 1
     #Reset portfolios
-    valueStock, valueBond, valuePureStock = initial()
+    valueStock, valueBond, valuePureStock, valueRStock, valueRBond = initial()
 
 print "Years per iteration: ", yearsPerIteration, "Total Iterations: ", totalIterations
-print "Mixed portfolios won: ", mixedPortfolioWins
+print "Mixed portfolio won: ", mixedPortfolioWins
 print "Pure stock won: ", pureStockWins
-print "Percent mixed wins: ", float(mixedPortfolioWins)/float(totalIterations)*100
+print "Rebalanced portfolio won: ", rebalancedPortfolioWins 
+print "Percent rebalanced wins: ", float(rebalancedPortfolioWins)/float(totalIterations)*100
